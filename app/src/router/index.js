635da666f653fff7a36887600c89408f36eda13a
qye <qye2022@gmail.com>
2022-12-26 20:18:17 -0700

    update router

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,28 +1,30 @@
 import VueRouter from "vue-router";
-import Home from '../pages/home'
-import Login from '../pages/login'
-import Register from '../pages/register'
-import Search from '../pages/search'
+import Home from '@/pages/home'
+import Login from '@/pages/login'
+import Register from '@/pages/register'
+import Search from '@/pages/search'
 
 export default new VueRouter({
-  route:{
-    name:'home',
-    path:'/home',
-    component:Home
-  },
-  route: {
-    name:'login',
-    path:"/login",
-    component:Login
-  },
-  route: {
-    name:'register',
-    path:"/register",
-    component:Register
-  },
-  route: {
-    name:'search',
-    path:"/search",
-    component:Search
-  },
+  routes:[
+    {
+      name:'home',
+      path:'/home',
+      component:Home
+    },
+    {
+      name:'login',
+      path:"/login",
+      component:Login
+    },
+    {
+      name:'register',
+      path:"/register",
+      component:Register
+    },
+    {
+      name:'search',
+      path:"/search",
+      component:Search
+    },
+  ]
 })
