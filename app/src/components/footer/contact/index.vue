7eb03a4d8c84d3172c02cbd88320bccd171f8c94
qye <qye2022@gmail.com>
2022-12-28 21:20:46 -0700

    static page - footer, split components

---
+++ app/src/components/footer/contact/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul class="contact">
+      <li>关于我们</li>
+      <li>联系我们</li>
+      <li>关于我们</li>
+      <li>商家入驻</li>
+      <li>营销中心</li>
+      <li>友情链接</li>
+      <li>关于我们</li>
+      <li>营销中心</li>
+      <li>友情链接</li>
+      <li>联系我们</li>
+    </ul>
+    <div class="address">
+      <address>地址：北京市昌平区鸿福科技综合楼6层</address>
+      <p>京ICP备19006430号</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'contact'
+}
+</script>
+
+<style scoped>
+  .contact {
+    margin-top: 30px;
+    text-align: center;
+  }
+  .contact li{
+    font-size: 12px;
+    display: inline;
+    padding: 0 10px;
+    border-right: 1px solid #000;
+  }
+
+  .address {
+    font-size: 12px;
+    text-align: center;
+    margin-top: 5px;
+  }
+
+  .address address {
+    margin-bottom: 15px;
+  }
+</style>
--- app/src/components/footer/index.vue
+++ app/src/components/footer/index.vue
@@ -40,29 +40,15 @@
         <img src="./images/wx_cz.jpg" alt="二维码">
       </Service>
     </div>
-  <ul class="contact">
-      <li>关于我们</li>
-      <li>联系我们</li>
-      <li>关于我们</li>
-      <li>商家入驻</li>
-      <li>营销中心</li>
-      <li>友情链接</li>
-      <li>关于我们</li>
-      <li>营销中心</li>
-      <li>友情链接</li>
-      <li>联系我们</li>
-  </ul>
-  <div class="address">
-    <address>地址：北京市昌平区鸿福科技综合楼6层</address>
-    <p>京ICP备19006430号</p>
-  </div>
+    <Contact></Contact>
   </div>
 </template>
 
 <script>
 import Service from './Service'
+import Contact from './contact'
 export default {
-  components:{Service},
+  components:{Service, Contact},
   data(){
     return {
       services: {
@@ -81,7 +67,6 @@ export default {
   .footer {
     background-color: #eaeaea;
     padding: 30px 60px;
-    
   }
 
   .services {
@@ -90,26 +75,5 @@ export default {
     justify-content: space-around;
     padding-bottom: 30px;
     border-bottom: 1px solid rgb(214, 210, 210);
-  }
-
-  .contact {
-    margin-top: 30px;
-    text-align: center;
-  }
-  .contact li{
-    font-size: 12px;
-    display: inline;
-    padding: 0 10px;
-    border-right: 1px solid #000;
-  }
-
-  .address {
-    font-size: 12px;
-    text-align: center;
-    margin-top: 5px;
-  }
-
-  .address address {
-    margin-bottom: 15px;
   }
 </style>
