acc0da7c81d00a534a94a829f11ce0acd8023502
qye <qye2022@gmail.com>
2023-01-02 14:45:34 -0700

    static comp - search - pagination

--- app/src/pages/search/index.vue
+++ app/src/pages/search/index.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
     <TypeNav class="type-nav"></TypeNav>
+    <Filters></Filters>
+    <div class="sort">
+      <li><a href="">综合↑</a></li>
+      <li><a href="">价格</a></li>
+      <li><a href="">新品</a></li>
+      <li><a href="">评价</a></li>
+    </div>
+    <div class="shop">
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+    </div>
+    <div class="shop">
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+      <ShoppingItem></ShoppingItem>
+    </div>
+    <Pagination></Pagination>
+    <Hot></Hot>
   </div>
 </template>
 
 <script>
+import Filters from './filters'
+import ShoppingItem from './shoppingItem'
+import Pagination from './pagination'
+import Hot from './hot'
 export default {
   name:'Search',
+  components:{Filters,ShoppingItem, Pagination, Hot}
 }
 </script>
 
@@ -14,4 +42,43 @@ export default {
   .type-nav {
     padding: 0 50px;
   }
+
+  .sort {
+    width: 1155px;
+    height: 40px;
+    margin: 0 auto;
+    border: 1px solid rgb(230,230,230);
+    box-shadow: 0 0 4px 1px rgb(230,230,230);
+    font-size: 12px;
+    margin-bottom: 20px;
+    display: flex;
+  }
+  .sort li{
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+  }
+
+  .sort li:first-child{
+    background-color: rgb(227,1,1);
+  }
+
+  .sort li a {
+    color: rgb(100,100,100)
+  }
+  .sort li a:hover {
+    color: black;
+  }
+
+  .sort li:first-child a {
+    color: white;
+  }
+
+  .shop {
+    display: flex;
+    width: 1155px;
+    margin: 0 auto;
+  }
+
 </style>
---
+++ app/src/pages/search/pagination/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="pagination">
+    <ul class="pagination-list">
+      <li class="prev"><a href="">上一页</a></li>
+      <ul class="page-number-list">
+        <li><a href="">1</a></li>
+        <li><a href="">2</a></li>
+        <li><a href="">3</a></li>
+      </ul>
+      <li class="next"><a href="">下一页</a></li>
+    </ul>
+    <p class="total-pages">共<span>3</span>页</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'pagination',
+}
+</script>
+
+<style scoped>
+  .pagination {
+    width: 1155px;
+    height: 40px;
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+    position: relative;
+    margin: 30px 0;
+    font-size: 13px;
+  }
+
+  .pagination .pagination-list,
+  .pagination .pagination-list .page-number-list {
+    display: flex;
+    height: 100%;
+  }
+
+  .pagination .pagination-list li{
+    border: 1px solid rgb(220,220,220);
+    /* display: flex; */
+    /* align-items: center; */
+    width: 100px;
+    height: 100%;
+    margin: 0 2px;
+  }
+
+  .pagination .pagination-list li a{
+    width: 100%;
+    height: 100%;
+  }
+
+  .pagination .pagination-list .page-number-list li{
+    width: 50px;
+  }
+  .pagination .pagination-list .page-number-list li a,
+
+  .pagination .pagination-list li a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .pagination .pagination-list li {
+    background-color: rgb(250,250,250);
+  }
+
+  .pagination .prev a,
+  .pagination .next a {
+    color: rgb(100,100,100);
+  }
+
+  .pagination .pagination-list li:hover{
+    background-color: rgb(220,220,220);
+  }
+
+  .pagination .pagination-list li:hover a{
+    color: white;
+  }
+
+  .pagination .total-pages {
+    position: absolute;
+    top: 5px;
+    right: 150px;
+  }
+</style>
