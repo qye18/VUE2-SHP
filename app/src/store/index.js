b91cfc6ae51f1d89e4ce217e9d45bc837a36943f
qye <qye2022@gmail.com>
2023-01-03 20:52:19 -0700

    store update - get and store search page info from server

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import home from './home'
-
+import search from './search'
 export default new Vuex.Store({modules:{
-  home,
+  home,search
 }})
 
---
+++ app/src/store/search/index.js
@@ -0,0 +1,37 @@
+import Vuex from 'vuex'
+import {reqSearchInfo} from '@/api'
+const state = {
+  searchList: {}
+};
+const actions = {
+  async searchList({commit},params={}) {
+    const result = await reqSearchInfo(params);
+    // console.log(result.data);
+    commit('SEARCHLIST', result.data);
+  }
+};
+const mutations = {
+  SEARCHLIST(state, searchList) {
+    state.searchList = searchList;
+  }
+};
+const getters = {
+  attrsList(state){
+    return state.searchList.attrsList;  
+  },
+  goodsList(state){
+    return state.searchList.goodsList;  
+  },
+  trademarkList(state){
+    return state.searchList.trademarkList;  
+  }
+};
+
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
+// reqSearchList
